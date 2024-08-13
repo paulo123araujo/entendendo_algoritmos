56606974793b0bd40a620c65e3220c496b1f306c
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2024-08-12 23:56:37 -0300

    do one more exercise for graph

--- graphs/bfs/lee-algorithm-shortest-path-in-a-maze.js
+++ graphs/bfs/lee-algorithm-shortest-path-in-a-maze.js
@@ -1,31 +1,34 @@
 // Link: https://www.techiedelight.com/lee-algorithm-shortest-path-in-a-maze/
 const matrix = require('./lee-algorithm-shortest-path.json');
 
-const allowedMoves = {
-  x: [-1, 0, 0, 1],
-  y: [0, -1, 1, 0],
-};
-
 function solve(src, dest, matrix) {
-  const queue = [];
-  const visited = new Set();
+    if (!matrix || !matrix.length) return -1;
 
-  queue.push({ x: src.x, y: src.y, dist: 0 });
-  while (queue.length > 0) {
-    const curr = queue.shift();
-    if (curr.x == dest.x && curr.y == dest.y) {
-      return curr.dist;
-    }
+    const queue = [];
+    const visited = new Set();
+    queue.push({ x: src.x, y: src.y, dist: 0 });
 
-    for (let i = 0; i < allowedMoves.x.length; i++) {
-      const x = curr.x + allowedMoves.x[i];
-      const y = curr.y + allowedMoves.y[i];
-      if (!visited.has({ x, y }) && x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length && matrix[x][y] == 1) {
-        visited.add({ x, y });
-        queue.push({ x, y, dist: curr.dist + 1 });
-      }
+    while (queue.length) {
+        const item = queue.shift();
+        if (
+            matrix[item.x] === undefined ||
+            matrix[item.x][item.y] === undefined ||
+            matrix[item.x][item.y] === 0 ||
+            visited.has(`${item.x}.${item.y}`)
+        ) {
+            continue;
+        }
+
+        if (item.x === dest.x && item.y === dest.y) {
+            return item.dist;
+        }
+
+        visited.add(`${item.x}.${item.y}`);
+        queue.push({ x: item.x - 1, y: item.y, dist: item.dist + 1 });
+        queue.push({ x: item.x, y: item.y - 1, dist: item.dist + 1 });
+        queue.push({ x: item.x + 1, y: item.y, dist: item.dist + 1 });
+        queue.push({ x: item.x, y: item.y + 1, dist: item.dist + 1 });
     }
-  }
 }
 
 console.log(`The shortest path is: ${solve({ x: 0, y: 0 }, { x: 7, y: 5 }, matrix)}`)
